ef869a5a36e6a7300b3ca00f6298d66c5d98dfcb
VaSanPue <53338433+VanxyS@users.noreply.github.com>
2020-03-08 13:31:11 +0100

    Cambio3

--- Proyecto/app/src/main/java/com/example/proyecto/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/proyecto/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(), "Cambio de ejemplo dos", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Cambio de ejemplo tres", Toast.LENGTH_LONG).show();
     }
 }
