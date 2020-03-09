3e308a13ad3ba15bf7b12595f9912c452b99d399
VaSanPue <53338433+VanxyS@users.noreply.github.com>
2020-03-09 11:01:08 +0100

    Registro2

--- Proyecto/app/src/main/java/com/example/proyecto/Registro.java
+++ Proyecto/app/src/main/java/com/example/proyecto/Registro.java
@@ -2,6 +2,7 @@ package com.example.proyecto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
@@ -14,5 +15,14 @@ public class Registro extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro);
 
+        Button registro = findViewById(R.id.registro);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
     }
 }
