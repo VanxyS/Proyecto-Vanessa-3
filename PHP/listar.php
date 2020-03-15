4e9061f8bde8dfe663cbe50caf1cb929e02291a6
VaSanPue <53338433+VanxyS@users.noreply.github.com>
2020-03-15 11:51:56 +0100

    Create listar.php

---
+++ PHP/listar.php
@@ -0,0 +1,57 @@
+﻿
+<?php
+
+	$c = mysqli_connect("localhost", "id12924384_vanessa", "vanessasantospuente", "id12924384_proyectodam");
+	
+	if($c==null)
+	{
+		echo "Fallo de conexion";
+	}
+	else
+	{		
+		$sql="SELECT * FROM luchadores";
+		$resultado=mysqli_query($c, $sql);
+	
+		if($resultado)
+		{
+	
+			$filas=mysqli_num_rows($resultado); //muestra cuantas filas tiene el resultado
+			
+			if($filas==0)
+			{
+				echo "Consultas no encontradas";
+			}
+			else
+			{
+				echo "<table border='1'><tr>";
+					echo "<th>ID</th>";
+					echo "<th>NOMBRE</th>";
+					echo "<th>APELLIDO</th>";
+					echo "<th>IDPESO</th>";
+					echo "<th>IDCATEGORIA</th>";
+				echo "</tr>";
+				
+				while($registro=mysqli_fetch_array($resultado))
+				{
+					echo "<tr>";
+					echo "<td>".$registro['id']."</td>";
+					echo "<td>".$registro['Nombre']."</td>";
+					echo "<td>".$registro['Apellidos']."</td>";
+					echo "<td>".$registro['idPeso']."</td>";
+					echo "<td>".$registro['idCategoria']."</td>";
+					echo "</tr>";
+				}
+				echo "</table>";
+			}
+			
+		}
+		else
+		{
+			$error=mysqli_error($c);
+			echo $error;
+			
+		}
+			
+		mysqli_close($c);
+	}
+?>
